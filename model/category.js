cd430d3fa68b49deff7a94d90bcfd3ef4fa3a81d
TANGQUANGTHONG <quangthong2808@gmail.com>
2024-10-13 10:54:55 +0700

    newne

--- model/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const categorySchema = new Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.models.Category || mongoose.model('Category', categorySchema);
